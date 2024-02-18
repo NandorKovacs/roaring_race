23efa5a012e0d7cd14df414bffd607ac9e839747
NandorKovacs <nandor1kovacs@gmail.com>
2024-02-18 02:52:37 +0100

    first assembled car

--- testbed/assembled.cpp
+++ testbed/assembled.cpp
@@ -22,7 +22,7 @@
 
 #include "test.h"
 namespace assembled {
-class Wheel{
+class Wheel {
  public:
   b2Body* wheel;
   Wheel(b2World* world, b2Body* ground, b2Vec2 position) {
@@ -32,7 +32,7 @@ class Wheel{
       wheel_body.position = position;
 
       b2PolygonShape poly;
-      poly.SetAsBox(0.3, 0.6, {0, 0}, 0);
+      poly.SetAsBox(0.5, 1.0, {0, 0}, 0);
 
       wheel = world->CreateBody(&wheel_body);
       wheel->CreateFixture(&poly, 1.0f);
@@ -45,8 +45,8 @@ class Wheel{
       jd.localAnchorA.SetZero();
       jd.localAnchorB = wheel->GetLocalCenter();
       jd.collideConnected = false;
-      jd.maxForce = 0.7f * wheel->GetMass() * 10;
-      jd.maxTorque = 0.5f * wheel->GetInertia() * 2 * 10;
+      jd.maxForce = 1.0f * wheel->GetMass() * 10;
+      jd.maxTorque = 0.7f * wheel->GetInertia() * 2 * 10;
       world->CreateJoint(&jd);
     }
   }
@@ -69,41 +69,63 @@ class Wheel{
   }
 };
 
+class Car {
+ public:
+  b2Body* car;
+
+  Car(b2World* world, b2Vec2 position) {
+    b2BodyDef car_body;
+    car_body.type = b2_dynamicBody;
+    car_body.position = position;
+
+    car = world->CreateBody(&car_body);
+    second_design();
+  }
+
+  void first_design() {
+    b2PolygonShape hull;
+    b2Vec2 points[6] = {{1, 2}, {3, 5}, {2, 9}, {-2, 9}, {-3, 5}, {-1, 2}};
+    hull.Set(points, 6);
+
+    b2PolygonShape tip;
+    b2Vec2 tp[3] = {{0, 0}, {1, 2}, {-1, 2}};
+    tip.Set(tp, 3);
+    car->CreateFixture(&hull, 1.0f);
+    car->CreateFixture(&tip, 1.0f);
+  }
+
+  void second_design() {
+    b2PolygonShape tip, middle, bottom;
+    b2Vec2 tip_vec[3] = {{0, 1.5}, {-1.5, 0}, {1.5, 0}};
+    b2Vec2 middle_vec[4] = {{1.5, 0}, {-1.5, 0}, {-1.25, -3.5}, {1.25, -3.5}};
+    b2Vec2 bottom_vec[4] = {{1.25, -3.5}, {-1.25, -3.5}, {-2, -6}, {2, -6}};
+
+
+    tip.Set(tip_vec, 3);
+    middle.Set(middle_vec, 4);
+    bottom.Set(bottom_vec, 4);
+
+    car->CreateFixture(&tip, 1.0f);
+    car->CreateFixture(&middle, 1.0f);
+    car->CreateFixture(&bottom, 1.0f);
+  }
+};
+
 class WheeledCar : public Test {
  public:
   b2Body* ground;
   Wheel* wheels[4];
-
-  void create_ground() {
-      b2BodyDef ground_body;
-      ground_body.type = b2_staticBody;
-      ground_body.position = b2Vec2{10, 0};
-      ground = m_world->CreateBody(&ground_body);
-
-      b2CircleShape shape;
-      shape.m_radius = 0.1;
-
-      b2FixtureDef fix;
-      fix.density = 0.0f;
-      fix.shape = &shape;
-
-      ground->CreateFixture(&fix);
-  }
-
+  Car* car;
   WheeledCar() {
     m_world->SetGravity(b2Vec2(0.0f, 0.0f));
 
     create_ground();
-    
-    for (int i = 0; i < 4; ++i) {
-      wheels[i] = new Wheel(m_world, ground, {(float)i, 2});
-    }
-  }
 
-  void update() {
-    for (int i = 0; i < 4; ++i) {
-      wheels[i]->update();
-    }
+    create_wheels();
+
+    create_car();
+
+    fix_wheels();
   }
 
   void Step(Settings& settings) {
@@ -113,6 +135,53 @@ class WheeledCar : public Test {
   }
 
   static Test* Create() { return new WheeledCar; }
+
+ private:
+  void create_ground() {
+    b2BodyDef ground_body;
+    ground_body.type = b2_staticBody;
+    ground_body.position = b2Vec2{10, 0};
+    ground = m_world->CreateBody(&ground_body);
+
+    b2CircleShape shape;
+    shape.m_radius = 0.1;
+
+    b2FixtureDef fix;
+    fix.density = 0.0f;
+    fix.shape = &shape;
+
+    ground->CreateFixture(&fix);
+  }
+
+  void create_wheels() {
+    wheels[0] = new Wheel(m_world, ground, {-1.5, 0});
+    wheels[1] = new Wheel(m_world, ground, {1.5, 0});
+    wheels[2] = new Wheel(m_world, ground, {-2, -6});
+    wheels[3] = new Wheel(m_world, ground, {2, -6});
+  }
+
+  void create_car() { car = new Car(m_world, {0, 0}); }
+
+  void fix_wheels() {
+    b2RevoluteJointDef fl, fr;
+    fl.Initialize(car->car, wheels[0]->wheel, car->car->GetWorldPoint({-1.5, 0}));
+    fr.Initialize(car->car, wheels[1]->wheel, car->car->GetWorldPoint({1.5, 0}));
+
+    b2WeldJointDef bl, br;
+    bl.Initialize(car->car, wheels[2]->wheel, car->car->GetWorldPoint({-2, -6}));
+    br.Initialize(car->car, wheels[3]->wheel, car->car->GetWorldPoint({2, -6}));
+  
+    m_world->CreateJoint(&fl);
+    m_world->CreateJoint(&fr);
+    m_world->CreateJoint(&bl);
+    m_world->CreateJoint(&br);
+  }
+
+  void update() {
+    for (int i = 0; i < 4; ++i) {
+      wheels[i]->update();
+    }
+  }
 };
 }  // namespace assembled
 
